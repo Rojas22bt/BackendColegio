22ae8abf2fbf13475e3de5ffb0165621d3f28f52
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-17 18:34:28 -0400

    fsdf

--- ColegioBK/settings.py
+++ ColegioBK/settings.py
@@ -79,15 +79,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ColegioBK.wsgi.application'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+# }
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+# }
 
 
 # Database
