8515e60c234ce0918dfbcccd5ecff4bf459bed1f
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-16 22:43:47 -0400

    SFw

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .controllers.controller_usuario import crear_usuario 
 
 urlpatterns = [
-    path('/crearUsuario', crear_usuario.as_view(),name='crear_usuario')
+    path('crearUsuario/', crear_usuario, name='crear_usuario')  # sin as_view()
 ]
