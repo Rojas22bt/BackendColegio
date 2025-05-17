6771811df6c71da76258a2375d902b2985fa6c41
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-17 09:29:05 -0400

    dsf

--- Usuarios/controllers/controller_permiso.py
+++ Usuarios/controllers/controller_permiso.py
@@ -18,3 +18,49 @@ def crear_rol(request):
             "mensaje": "Error al crear el rol",
             "errores": serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def actualizar_rol(request,id):
+    try:
+        rol = Rol.objects.get(id=id)
+    except Rol.DoesNotExist:
+        return Response(
+            {"mensaje": "Rol no encontrado"},
+            status=status.HTTP_404_NOT_FOUND
+            )
+    serializer = RolSerializers(rol, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "mensaje": "Rol actualizado correctamente",
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+    return Response({
+        "mensaje": "Error al actualizar el rol",
+        "errores": serializer.errors
+    }, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['DELETE'])
+def eliminar_rol(request,id):
+    try:
+        rol = Rol.objects.get(id=id)
+        rol.delete()
+        return Response(
+            {"mensaje": "Rol elimina"},
+            status=status.HTTP_200_OK
+        )
+    except Rol.DoesNotExist:
+      return Response({"mensaje": "Rol no encontrado"}, status=status.HTTP_404_NOT_FOUND)   
+
+@api_view(['GET'])
+def obtener_roles(request):
+    roles = Rol.objects.all()
+    serializers = RolSerializers(roles, many=True)
+    return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def obtener_roles_activos(request):
+    roles = Rol.objects.filter(estado=True)
+    serializer = RolSerializers(roles, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .controllers.controller_usuario import crear_usuario 
-from .controllers.controller_permiso import crear_rol
+from .controllers.controller_permiso import crear_rol,actualizar_rol,eliminar_rol,obtener_roles,obtener_roles_activos
 
 urlpatterns = [
     path('crearUsuario/', crear_usuario, name='crear_usuario'),
-    path('crearRol/', crear_rol, name='crear_rol')
+    
+    #------URL PARA ROL-----
+    path('crearRol/', crear_rol, name='crear_rol'),
+    path('obtenerRoles/', obtener_roles),
+    path('obtenerRolesActivos/', obtener_roles_activos),
+    path('actualizarRol/<int:id>/', actualizar_rol),
+    path('eliminarRol/<int:id>/', eliminar_rol),
 ]
