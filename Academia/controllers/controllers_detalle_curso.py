90941131ab99e76ecb077ac5b6ed05e175a943c9
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-23 00:18:56 -0400

    ya

--- Academia/controllers/controllers_detalle_curso.py
+++ Academia/controllers/controllers_detalle_curso.py
@@ -63,7 +63,6 @@ def actualizar_detalle_curso_materia(request):
 
 @api_view(['DELETE'])
 def eliminar_detalle_curso_materia(request):
-    print(request)
     materia_id = request.data.get('materia')
     curso_id = request.data.get('curso')
 
