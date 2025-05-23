8c9f8508f6e972d143313f180ead8e294451aa5a
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-23 15:46:34 -0400

    as

--- Academia/controllers/controllers_detalle_materia.py
+++ Academia/controllers/controllers_detalle_materia.py
@@ -15,11 +15,11 @@ def obtener_descripcion_completa(request):
         horarios_serializados = DescripcionHorarioSerializer(horarios, many=True).data
 
         #agregarNombreProfesor
-        profesor = Profesor.objects.get(id=descripcion_serializada["profesor"])
-        descripcion_serializada["profesor_nombre"] = profesor.nombre
+        # profesor = Profesor.objects.get(id=descripcion_serializada["profesor"])
+        # descripcion_serializada["profesor_nombre"] = profesor.nombre
         #agregamosNombreMateria
-        materia = Materia.objects.get(id=descripcion_serializada["materia"])
-        descripcion_serializada["materia_nombre"] = materia.nombre
+        # materia = Materia.objects.get(id=descripcion_serializada["materia"])
+        # descripcion_serializada["materia_nombre"] = materia.nombre
 
         resultado.append({
             "descripcion": descripcion_serializada,
