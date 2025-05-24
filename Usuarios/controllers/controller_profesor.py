f3cea74e9a87438f3b42bfc8733e4aa191126126
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-24 16:33:40 -0400

    ojala de

--- Usuarios/controllers/controller_profesor.py
+++ Usuarios/controllers/controller_profesor.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from BaseDatosColegio.models import Usuario,Profesor,Alumno,DescripcionMateria,HorarioMateria
-from Academia.serializers import DescripcionHorarioSerializer,DescripcionMateriaSerializer
+from BaseDatosColegio.models import DescripcionMateria,HorarioMateria,Materia
+from Academia.serializers import DescripcionHorarioSerializer,DescripcionMateriaSerializer,MateriaSerializer
 
 @api_view(['GET'])
 def obtener_materia_horario_profesor(request, id):
@@ -12,6 +12,12 @@ def obtener_materia_horario_profesor(request, id):
         horarios = HorarioMateria.objects.get(descripcion_materia=detalle)
         serializer = DescripcionHorarioSerializer(horarios).data
         serializer2 = DescripcionMateriaSerializer(detalle).data
+        
+                #agregamosNombreMateria
+        materia_id = serializer2["materia"]
+        materia = Materia.objects.get(id=materia_id)
+        obtenerMateria = MateriaSerializer(materia).data
+        serializer2["materia_nombre"] = obtenerMateria["nombre"]
         resultado.append({
             "descripcion":serializer2,
             "horarios":serializer
