4e2603c93e3973f56eee99db44e646669f7902e0
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-24 15:53:13 -0400

    probando

--- Usuarios/controllers/controller_usuario.py
+++ Usuarios/controllers/controller_usuario.py
@@ -6,18 +6,31 @@ from Usuarios.serializers import UsuarioSerializer
 
 @api_view(['POST'])
 def crear_usuario(request):
-    serializer = UsuarioSerializer(data=request.data)
+    data = request.data.copy()  # Copia modificable del request
+
+    password = data.get('password')
+    rol_id = int(data.get('rol', 0))
+
+    # Solo si no se proporciona contraseña
+    if not password:
+        if rol_id == 2:  # Profesor
+            data['password'] = data.get('ci', '')
+        elif rol_id == 5:  # Alumno
+            data['password'] = data.get('matricula', '')
+
+    serializer = UsuarioSerializer(data=data)
     if serializer.is_valid():
         usuario = serializer.save()
-        
-        rol_id =int(request.data.get('rol', 0))
+
         if rol_id == 2:
-            Profesor.objects.create(profesor=usuario, especialidad=request.data.get('especialidad', ''))
+            Profesor.objects.create(profesor=usuario, especialidad=data.get('especialidad', ''))
         elif rol_id == 5:
-            Alumno.objects.create(alumno=usuario, matricula=request.data.get('matricula', ''))
+            Alumno.objects.create(alumno=usuario, matricula=data.get('matricula', ''))
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def obtener_usuarios(request):
