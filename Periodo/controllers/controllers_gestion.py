911f2ae7bdc74232f7779b6e4f0836dfc26a0083
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-25 10:56:29 -0400

    vcv

--- Periodo/controllers/controllers_gestion.py
+++ Periodo/controllers/controllers_gestion.py
@@ -4,13 +4,19 @@ from rest_framework import status
 from BaseDatosColegio.models import Gestion,Trimestre,DetalleTrimestre
 from Periodo.serializers import GestionSerializers,DetalleTrimestreSerializer
 
-
 @api_view(['POST'])
 def crear_gestion(request):
+    anio_escolar = request.data.get("anio_escolar")
+
+    # Verificar si ya existe una gestión con ese año
+    gestion_existente = Gestion.objects.filter(anio_escolar=anio_escolar).first()
+    if gestion_existente:
+        return Response({"error": "Ya existe este año"}, status=status.HTTP_400_BAD_REQUEST)
+
     serializer = GestionSerializers(data=request.data)
     if serializer.is_valid():
-        serializer.saver()
-        return Response({"data":serializer.data},status=status.HTTP_200_OK)
-    return Response({"erros":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+    
+    return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
