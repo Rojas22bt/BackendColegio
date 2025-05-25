a6c2bff3e27ccbe332fd8691c880bb0fea13610c
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-25 01:02:26 -0400

    hola

---
+++ Periodo/controllers/controller_notificacion.py
@@ -0,0 +1,35 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from BaseDatosColegio.models import Notificacion, Usuario
+from Periodo.serializers import NotificacionSerializers
+
+@api_view(['POST'])
+def crear_notificacion_uni(request,id):
+    try:
+        usuario = Usuario.objects.get(id=id)
+    except Usuario.DoesNotExist:
+        return Response({"mensaje":"usuario no encontrado"}, status=status.HTTP_400_BAD_REQUEST)
+    data = request.data.copy()
+    data["usuario"] = usuario.id
+    serializer = NotificacionSerializers(data,many=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"mensaje":"notificacion creado"}, status=status.HTTP_201_CREATED)
+    return Response({"error":serializer.errors},status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def obtener_notificacion_uni(request,id):
+    try:
+        usuario = Usuario.objects.get(id=id)
+    except Usuario.DoesNotExist:
+        return Response({"mensaje":"usuario no encontrado"}, status=status.HTTP_400_BAD_REQUEST)
+    notificaciones = Notificacion.objects.filter(usuario=id)
+    resultado= []
+    for noti in notificaciones:
+        serializer = NotificacionSerializers(noti).data
+        resultado.append({
+            "notificaciones":serializer
+        }
+        )
+    return Response(resultado, status=status.HTTP_200_OK)
--- Periodo/urls.py
+++ Periodo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from Periodo.controllers.controller_trimestre import crear_trimestre,obtener_trimestres,actualizar_trimestre,eliminar_trimestre
 from Periodo.controllers.controllers_gestion import crear_gestion
-
+from Periodo.controllers.controller_notificacion import crear_notificacion_uni
 urlpatterns = [
     path('crear-trimestre/',crear_trimestre),
     path('obtener-trimestres/',obtener_trimestres),
     path('actualizar-trimestre/<int:id>/',actualizar_trimestre),
     path('eliminar-trimestre/<int:id>/',eliminar_trimestre),
     path('crear-gestion/',crear_gestion),
+    path('crear-notificacion-uni/<int:id>/',crear_notificacion_uni),
 ]
