fc7926dd9cd85ffc5f0078a4cb4cf6544afe165b
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-30 00:02:03 -0400

    pk

--- Periodo/urls.py
+++ Periodo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from Periodo.controllers.controller_trimestre import crear_trimestre,obtener_trimestres,actualizar_trimestre,eliminar_trimestre
 from Periodo.controllers.controllers_gestion import crear_gestion,obtener_gestiones_completas
 from Periodo.controllers.controller_notificacion import crear_notificacion_uni,obtener_notificacion_uni,actualizar_notificacion_uni,crear_notificacion_rol
+from Periodo.controllers.controller_participacion import crear_participacion
 urlpatterns = [
     path('crear-trimestre/',crear_trimestre),
     path('obtener-trimestres/',obtener_trimestres),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('obtener-notificacion-uni/<int:id>/',obtener_notificacion_uni),
     path('actualizar-notificacion-uni/<int:id_notificacion>/',actualizar_notificacion_uni),
     path('crear-notificacion-rol/',crear_notificacion_rol),
+    path('crear-participacion/',crear_participacion),
 ]
