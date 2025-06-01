e0faec67f9ddd64cc1251aa869cb1e6c26346007
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-06-01 15:43:37 -0400

    gfd

--- Usuarios/controllers/controllers_alumno.py
+++ Usuarios/controllers/controllers_alumno.py
@@ -7,11 +7,11 @@ from Usuarios.serializers import UsuarioSerializer,AlumnoSerializer
 
 @api_view(['GET'])
 def obtener_alumnos(request,gestion,id_paralelo):
-    alumnos = Usuario.objects.filter(
-        alumno__alumnocursoparalelo__curso_paralelo_id=id_paralelo,
-        alumno__alumnocursoparalelo__gestion_id=gestion
+    alumnos = Alumno.objects.filter(
+        alumnocursoparalelo__curso_paralelo_id=id_paralelo,
+        alumnocursoparalelo__gestion_id=gestion
     )
-    serializer = UsuarioSerializer(alumnos,many=True)
+    serializer = AlumnoSerializer(alumnos,many=True)
     return Response(serializer.data,status=status.HTTP_200_OK)
     
     
