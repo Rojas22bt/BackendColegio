cc420b2cf19d64e88b774d960278e04180648328
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-06-02 11:15:31 -0400

    url

--- Periodo/urls.py
+++ Periodo/urls.py
@@ -3,6 +3,7 @@ from Periodo.controllers.controller_trimestre import crear_trimestre,obtener_tri
 from Periodo.controllers.controllers_gestion import crear_gestion,obtener_gestiones_completas
 from Periodo.controllers.controller_notificacion import crear_notificacion_uni,obtener_notificacion_uni,actualizar_notificacion_uni,crear_notificacion_rol
 from Periodo.controllers.controller_participacion import crear_participacion,obtener_participaciones
+from Periodo.controllers.controller_tokens import guardar_token
 urlpatterns = [
     path('crear-trimestre/',crear_trimestre),
     path('obtener-trimestres/',obtener_trimestres),
@@ -16,4 +17,5 @@ urlpatterns = [
     path('crear-notificacion-rol/',crear_notificacion_rol),
     path('crear-participacion/',crear_participacion),
     path('obtener-participacion/<int:alumno>/<int:materia>/',obtener_participaciones),
+    path('guardar-nuevo-token/',guardar_token),
 ]
