3f774888ebefda272d9dd762c66f9e1b0e02728a
Joan Dr <joandanielrr@gmail.com>
2025-06-02 14:01:00 -0400

    f

--- ColegioBK/settings.py
+++ ColegioBK/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = True
 CORS_ALLOWED_ORIGINS = [
     "https://venerable-palmier-c7fb94.netlify.app",
 ]
