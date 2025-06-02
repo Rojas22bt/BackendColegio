cdf62360a91076ec5d25111be132acf2afb51a00
Joan Dr <joandanielrr@gmail.com>
2025-06-02 14:04:52 -0400

    f

--- Periodo/controllers/controller_notificacion.py
+++ Periodo/controllers/controller_notificacion.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -46,6 +47,7 @@ def enviar_notificacion_firebase(titulo, mensaje, token):
         return {"enviado": False, "motivo": f"Error al enviar: {e}"}
 
 
+@csrf_exempt
 @api_view(['POST'])
 def crear_notificacion_uni(request, id):
     try:
